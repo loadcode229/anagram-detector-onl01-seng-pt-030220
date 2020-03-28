6ef39bcc72e78d9b6ad5f7619b9090c1f519ac7c
Adam Priest <sepone229@gmail.com>
2020-03-28 14:05:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :words
+  
+  def initialize(word)
+    @words = word
+  end
+  
+  def match_array
+    array = @words.match(%w(word))
+  end
+  
+  
+  
+  
+end
